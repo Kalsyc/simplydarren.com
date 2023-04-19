412d0b144e37fc786ea7771209718de2bbd61fb2
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2023-04-19 14:13:32 +0800

    Add in settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+  import SimplyDarrenIcon from '$lib/assets/svg/SimplyDarrenIcon.svelte';
+</script>
+
 <h1>Welcome to simplydarren.com</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<SimplyDarrenIcon />
