b779b158ca2576bfdedccd9262de951ae8583fbc
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2023-04-22 00:25:31 +0800

    Add slug page for projects

---
+++ src/routes/projects/[projectId]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import { projectsObj } from '$lib/constants/projects';
+
+  const projectInfo = projectsObj[$page.params['projectId']];
+</script>
+
+<svelte:head>
+  <title>{projectInfo.title}</title>
+</svelte:head>
+
+<h1>Description coming soon!</h1>
+<div>{projectInfo.title}</div>
+
+<style>
+</style>
