f1e3680979506c06a00b9685874ad81e8c711000
Darren Sim <45655404+Kalsyc@users.noreply.github.com>
2023-04-23 18:32:11 +0800

    Turn game links to buttons

--- src/lib/constants/games.ts
+++ src/lib/constants/games.ts
@@ -7,18 +7,18 @@ export interface GameInfo {
 }
 
 export const games: { [id: string]: GameInfo } = {
-  '0': {
+  CULLinary: {
     gameBuildLink: 'https://simplydarren-test.s3.ap-southeast-1.amazonaws.com/CULLinary_WebGL_Build_Prod/index.html',
     frameName: 'CULLinary',
     frameTitle: 'CULLinary',
     headTitle: 'CULLinary',
-    id: '0',
+    id: 'CULLinary',
   },
-  '1': {
+  CULLinary2: {
     gameBuildLink: 'https://simplydarren-test.s3.ap-southeast-1.amazonaws.com/CULLv2_WebGLBuild_Prod/index.html',
     frameName: 'CULLinary 2',
     frameTitle: 'CULLinary 2',
     headTitle: 'CULLinary 2',
-    id: '1',
+    id: 'CULLinary2',
   },
 };
